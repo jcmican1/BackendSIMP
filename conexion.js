9174903d91193f6fdb40b3d4d88717faff2a272f
jcmican1 <119264781+jcmican1@users.noreply.github.com>
2024-03-16 18:07:46 -0500

    node start

--- conexion.js
+++ conexion.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    database: 'simpUv',
+    host: 'monorail.proxy.rlwy.net',
+    database: 'railway',
     user: 'root',
-    password: '',
+    password: 'JrzHHaWCwOVslyfcAGDcPgRuzbeIBHXU',
+    port: 45773
 });
 
 conexion.connect(error => {
